fd45f56070009b67b792efc4bd95cc98f0b7a054
Antonio Lain <antlai@cafjs.com>
2017-01-02 00:49:02 -0800

    Add relative files support

--- public/js/revealUtil.js
+++ public/js/revealUtil.js
@@ -2,9 +2,31 @@ var Reveal = require('../reveal/js/reveal.js');
 var AppActions = require('./actions/AppActions');
 
 var hljs = require('highlight.js');
+var request = require('superagent');
 
 var MARKDOWN_SECTION_ID = 'markdownSectionId';
 
+var patchMarkdown = function(data, cb) {
+    var markElem = document && document.getElementById(MARKDOWN_SECTION_ID);
+    if (markElem && data.markdownURL) {
+        request
+            .get(data.markdownURL)
+            .end(function(err, res) {
+                if (err) {
+                    cb(err);
+                } else {
+                    var prefix = data.markdownURL.split('/')[0];
+                    var text = res.text.replace(/process.env.CA_NAME/g, prefix);
+                    text = 'data:text/x-markdown;charset=utf-8;base64,' +
+                        window.btoa(text);
+                    markElem.setAttribute('data-markdown', text);
+                    cb(null);
+                }
+            });
+    } else {
+        cb(new Error('Missing markdown section'));
+    }
+};
 
 exports.init = function(ctx, data) {
     var firstSlide = null;
@@ -28,34 +50,44 @@ exports.init = function(ctx, data) {
         }
     });
 
-    var markElem = document && document.getElementById(MARKDOWN_SECTION_ID);
-    if (markElem && data.markdownURL) {
-        markElem.setAttribute('data-markdown', data.markdownURL);
-    }
-
-    Reveal.initialize({
-        history: false,
-        progress: false,
-        controls: false,
-        center: false,
-        slideNumber: true,
-        transition: 'none',
-        transitionSpeed: 'fast',
-        dependencies: [
-	    { src: 'reveal/plugin/markdown/marked.js',
-              condition: function() {
-                  return !!document.querySelector( '[data-markdown]' );
-              }
-            },
-            { src: 'reveal/plugin/markdown/markdown.js',
-              condition: function() {
-                  return !!document.querySelector( '[data-markdown]' );
-              }
+    patchMarkdown(data, function(err) {
+        if (err) {
+            console.log(err);
+        } else {
+            if (data.print) {
+	        var link = document.createElement( 'link' );
+	        link.rel = 'stylesheet';
+	        link.type = 'text/css';
+	        link.href = 'reveal/css/print/pdf.css';
+	        document.getElementsByTagName('head')[0].appendChild(link);
             }
-	]
-    });
+            Reveal.initialize({
+                history: false,
+                progress: false,
+                controls: false,
+                center: false,
+                slideNumber: true,
+                transition: 'none',
+                transitionSpeed: 'fast',
+                dependencies: [
+	            {
+                        src: 'reveal/plugin/markdown/marked.js',
+                        condition: function() {
+                            return !!document.querySelector('[data-markdown]');
+                        }
+                    },
+                    {
+                        src: 'reveal/plugin/markdown/markdown.js',
+                        condition: function() {
+                            return !!document.querySelector('[data-markdown]');
+                        }
+                    }
+	        ]
+            });
 
-    firstSlide = (data.currentPage &&
-                  (typeof data.currentPage.num === 'number') ?
-                  data.currentPage.num : null);
+            firstSlide = (data.currentPage &&
+                          (typeof data.currentPage.num === 'number') ?
+                          data.currentPage.num : null);
+        }
+    });
 };
